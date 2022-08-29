804f93bd9163216c8125894cae2838716a43172b
Anastasiia <Anastasiia>
2022-08-29 15:48:05 +0200

    Change everything

--- main.py
+++ main.py
@@ -7,43 +7,41 @@ translator = deepl.Translator(auth_key="75f0db24-ae8f-fba4-79da-e82896e90bac:fx"
 
 
 @bot.message_handler(commands=['start'])
-def start(message):
-    bot.send_message(message.chat.id, "<b>Привет, бублик! "
-                                      "Введи предложение на русском для перевода на английский</b>", parse_mode='html')
+def set_buttons(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
+    button1 = types.KeyboardButton(text='English')
+    button2 = types.KeyboardButton(text='French')
+    markup.add(button1)
+    markup.add(button2)
+    bot.send_message(message.chat.id, text='<b>Привет, бублик! На какой язык нужно перевести?</b>', parse_mode='html', reply_markup=markup)
 
 
 @bot.message_handler(commands=['help'])
 def help(message):
-    bot.send_message(message.chat.id, "/start - запуск бота\n/help - команды бота\n/translate_to - перевод на")
-
-
-@bot.message_handler(commands=['translate_to'])
-def translate_to(message):
-    bot.send_message(message.chat.id, text="На какой язык нужно перевести?", reply_markup=set_buttons())
-
-
-@bot.message_handler(content_types=['text'])
-def set_buttons():
-    markup = types.InlineKeyboardMarkup(row_width=1)
-    button1 = types.InlineKeyboardButton(text='English', callback_data='cd_English')
-    button2 = types.InlineKeyboardButton(text='Russian', callback_data='cd_Russian')
-    markup.add(button1)
-    markup.add(button2)
-    return markup
+    bot.send_message(message.chat.id, "/start - запуск бота\n/help - команды бота")
 
 
 @bot.callback_query_handler(func=lambda call: True)
 def answerEnglish(call):
-    if call.data == 'cd_English':
-        bot.send_message(call.chat.id, 'Translate to English enabled')
+    if call.data == "English":
         translateEnglish()
+        bot.send_message(call.chat.id, 'Translate to English enabled')
 
 
-@bot.callback_query_handler(func=lambda call: True)
-def answerRussian(call):
-    if call.data == 'cd_Russian':
-        bot.send_message(call.chat.id, 'Translate to Russian enabled')
-        translateRussian()
+@bot.message_handler(content_types=['text'])
+def answerFrench(call, message):
+    if call.text == "French":
+        bot.send_message(call.chat.id, 'Перевод на французкий включен. Напиши предложение для перевода')
+        savedPhrase = message.text()
+
+        bot.register_next_step_handler(savedPhrase, translateFrench)
+        
+    
+@bot.message_handler(content_types=['text'])
+def translateFrench(message, savedPhrase=None):
+    translateText = translator.translate_text(savedPhrase, target_lang="FR")
+    bot.send_message(message.chat.id, str(translateText))
+
 
 
 @bot.message_handler(content_types=['text'])
@@ -51,14 +49,6 @@ def translateEnglish(message):
     savedPhrase = message.text
 
     translateText = translator.translate_text(savedPhrase, target_lang="EN-US")
-    bot.send_message(message.chat.id, str(translateText))
-
-
-@bot.message_handler(content_types=['text'])
-def translateRussian(message):
-    savedPhrase = message.text
-
-    translateText = translator.translate_text(savedPhrase, target_lang="RU")
     bot.send_message(message.chat.id, str(translateText))
 
 
